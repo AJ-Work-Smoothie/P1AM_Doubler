da15290bee6112845d3dd4a99ad974dd8400a9ab
AJ-Work-Smoothie <aj@us-lmp.com>
2023-10-05 15:08:55 -0400

    Blankets

--- src/src.ino
+++ src/src.ino
@@ -1,3 +1,26 @@
-void setup() {}
+#include "P1AM.h"
 
-void loop() {}
+void setup() 
+{
+  pinMode(7, OUTPUT); 
+  digitalWrite(7, HIGH);
+
+  Serial.begin(115200);
+  P1.init();
+
+  
+
+}
+
+void loop() 
+{
+
+  SPI.begin();
+  SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));
+	digitalWrite(7, LOW);
+  SPI.transfer('K');
+	digitalWrite(7, HIGH);
+	SPI.endTransaction();
+	SPI.end();
+    
+}
